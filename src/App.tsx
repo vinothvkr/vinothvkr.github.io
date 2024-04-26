bef81a6d8bc5194633c4017abc88b5cab88dff15
vinothvkr <vinothvkr@hotmail.com>
2024-04-26 15:34:26 +0530

    add email and change icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { Avatar, Box, Container, IconButton, Stack, Typography } from '@mui/material'
-import { GitHub, X } from '@mui/icons-material';
+import { Avatar, Box, Container, IconButton, Stack, Tooltip, Typography } from '@mui/material';
 
 // FONTS
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { IconBrandBlogger, IconBrandGithub, IconBrandX, IconMail } from '@tabler/icons-react';
 
 function App() {
 
@@ -16,20 +16,34 @@ function App() {
         <Stack sx={{height: 'inherit'}} justifyContent='center' alignItems='center' spacing={3}>
           <Avatar alt='Vinoth Kumar Rajendran' src='/avatar.jpg' sx={{width: 200, height:200}} />
           <Stack alignItems='center'>
-            <Typography variant='h3' component='h1' color="#1A2027">
+            <Typography variant='h3' component='h1' color="#1A2027" textAlign="center">
               Vinoth Kumar Rajendran
             </Typography>
-            <Typography variant='h5' component='h2' color="#1A2027" sx={{mt: 0}}>
+            <Typography variant='h5' component='h2' color="#1A2027" textAlign="center" sx={{mt: 0}}>
               Senior Software Engineer
             </Typography>
           </Stack>
           <Stack direction='row' spacing={1}>
-            <IconButton href='https://github.com/vinothvkr' target='_blank'>
-              <GitHub />
-            </IconButton>
-            <IconButton href='https://twitter.com/_vinothvkr' target='_blank'>
-              <X />
-            </IconButton>
+            <Tooltip title="Email">
+              <IconButton href='mailto:vinothvkr@hotmail.com'>
+                <IconMail />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Blog">
+              <IconButton href='https://vinothvkr.com/blog' target='_blank'>
+                <IconBrandBlogger />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Github">
+              <IconButton href='https://github.com/vinothvkr' target='_blank'>
+                <IconBrandGithub />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Twitter">
+              <IconButton href='https://twitter.com/_vinothvkr' target='_blank'>
+                <IconBrandX />
+              </IconButton>
+            </Tooltip>
           </Stack>
         </Stack>
       </Container>
