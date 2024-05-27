fe59a4b68b8285d9464fdb6728cfd61cccc88ae9
vinothvkr <vinothvkr@hotmail.com>
2024-05-27 12:50:09 +0530

    move fonts to main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,6 @@ import ThemeProvider from './theme';
 import Router from './routes';
 import { SettingsProvider } from './components/settings/SettingsContext';
 
-// FONTS
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
 function App() {
   return (
     <BrowserRouter>
