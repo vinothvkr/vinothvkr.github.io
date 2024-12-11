2829956c4956e10cb5028038f481179d4115abae
vinothvkr <vinothvkr@hotmail.com>
2024-12-11 15:55:07 +0530

    layout changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,19 +26,19 @@ export default function RootLayout({
       <body className={`${roboto.variable} font-roboto flex min-h-full antialiased`}>
         <div className="bg-base-100 drawer md:drawer-open">
           <input id="drawer" type="checkbox" className="drawer-toggle" />
-          <main className="drawer-content">
-            <NavBar />
-            <div className="max-w-[100vw] px-6 pb-16 xl:pr-2">
-              <div className="flex flex-col-reverse justify-between gap-6 xl:flex-row">
-                <div className="prose prose-sm md:prose-base w-full max-w-4xl flex-grow">
-                  {children}
-                </div>
-                <div className="hidden xl:flex">
-                  <aside className="sticky top-20 my-6 xl:mx-2 xl:my-0 xl:mt-32"></aside>
+          <div className="drawer-content">
+            <div className="container mx-auto px-4">
+              <NavBar />
+              <div className="max-w-[100vw] px-6 pb-16 xl:pr-2">
+                <div className="flex flex-col-reverse justify-between gap-6 xl:flex-row">
+                  <main className="prose prose-sm md:prose-base w-full max-w-3xl flex-grow">
+                    {children}
+                  </main>
+                  <aside className="sticky hidden xl:flex top-20 my-6 xl:mx-2 xl:my-0 xl:mt-32"></aside>
                 </div>
               </div>
             </div>
-          </main>
+          </div>
           <Sidebar />
         </div>
       </body>
