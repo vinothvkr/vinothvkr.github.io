6ce5d581adbaab08ff1e6cafa22109df2848f2bf
vinothvkr <vinothvkr@hotmail.com>
2024-12-24 12:40:53 +0530

    truncate title in post feed

--- src/components/postcard/PostCard.tsx
+++ src/components/postcard/PostCard.tsx
@@ -12,17 +12,18 @@ type Post = {
 };
 
 export default function BlogPostCard({ slug, category, date, title, excerpt, image }: Post) {
+  const truncated_title = title && title.length > 45 ? `${title?.slice(0, 45)}...` : title;
   const truncated_excerpt =
     excerpt && excerpt.length > 128 ? `${excerpt?.slice(0, 128)}...` : excerpt;
   return (
     <article>
       <Link href={`/posts/${slug}`}>
         <div className="card md:card-side bg-base-300 md:flex-row-reverse">
-          <figure className="md:w-1/3 md:rounded-tl-[unset] md:rounded-tr-[inherit] md:rounded-bl-[unset] md:rounded-br-[inherit]">
+          <figure className="md:w-2/5 md:rounded-tl-[unset] md:rounded-tr-[inherit] md:rounded-bl-[unset] md:rounded-br-[inherit]">
             <img alt={title} src={image} />
           </figure>
-          <div className="card-body md:w-2/3">
-            <h2 className="card-title font-normal">{title}</h2>
+          <div className="card-body md:w-3/5">
+            <h2 className="card-title font-normal">{truncated_title}</h2>
             <p className="mt-0 text-sm text-gray-500">{truncated_excerpt}</p>
             <div className="card-actions justify-start space-x-4 text-sm text-gray-500">
               <div className="flex space-x-1">
